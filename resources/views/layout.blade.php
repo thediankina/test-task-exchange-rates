b7402169e6206ef1c0526c6312429800082beb00
Diana <digaliulina@gmail.com>
2022-07-19 17:19:07 +0500

    Create layout and ajax update settings

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Главная страница</title>
+    <title>@yield('title')</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
-
+@yield('content')
 </body>
 </html>
---
+++ resources/views/script/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layout')
+
+@section('title')
+    Настройки отслеживания
+@endsection
+
+@section('content')
+<form method="post" id="tracing-form" action="settings/update">
+    @csrf
+    <div class="checkbox-codes">
+        @foreach($codes as $code)
+            <input type="checkbox" id="code" name="ids[]"
+                   value="{{ $code['id'] }}" {{ $code['trace'] ? 'checked' : '' }}>
+            <label for="code">{{ $code['char_code'] }}</label>
+        @endforeach
+    </div>
+    <button type="submit">Выбрать</button>
+</form>
+
+<a href="/rates">Values</a>
+
+<script>
+    $('#tracing-form').on('submit', function (event) {
+        event.preventDefault();
+
+        let ids = [];
+        $('input:checked').each(function () {
+            ids.push(this.value);
+        });
+
+        $.ajax({
+            url: "/settings/update",
+            type: "POST",
+            data: {
+                "_token": "{{ csrf_token() }}",
+                ids: ids,
+            },
+        });
+    });
+</script>
+@endsection
